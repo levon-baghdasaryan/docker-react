f30cc254c22488cb4e79ac8fad35243918e62ff2
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2021-06-25 01:02:48 +0400

    changed text in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature breanch.
+          Learn React
         </a>
       </header>
     </div>
